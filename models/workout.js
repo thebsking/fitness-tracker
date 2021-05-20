2b79dc096025fa7a7908b49ff589a7039f06271b
Brandon King <brandonscottking@gmail.com>
2021-05-20 08:46:14 -0400

    updated Workout model

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,35 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+const workoutSchema = new Schema({
+  day: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+      }, 
+      name: {
+        type: String
+      }, 
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: { 
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      }
+    }
+  ]
 });
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = Transaction;
+module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
 
-router.post('/api/workout', ({body}, res) => {
+router.post('/api/workouts', ({body}, res) => {
     Workout.create(body)
         .then(dbTransaction => {
             res.json(dbTransaction);
