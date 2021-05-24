46891f622096999ce630eaadb1af1b63cfec8f23
Brandon King <brandonscottking@gmail.com>
2021-05-24 19:32:53 -0400

    fixing syntax for deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('../models/Workout.js');
+const Workout = require('../models/Workout');
 
 router.get('/api/workouts', (req, res) => {
     Workout.find({})
