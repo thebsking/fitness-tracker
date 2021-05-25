73b619cae7f47829ded536b5cc670f592eeafe9b
Brandon King <brandonscottking@gmail.com>
2021-05-24 20:47:26 -0400

    fixed seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Workout = require('../models');
+const db = require('../models');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
