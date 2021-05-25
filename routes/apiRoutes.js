9ab71fe3f2a25621ebf89ea310075a0033ecccf4
Brandon King <brandonscottking@gmail.com>
2021-05-24 20:04:20 -0400

    reset require routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('/app/models/Workout');
+const Workout = require('../models/Workout');
 
 router.get('/api/workouts', (req, res) => {
     Workout.find({})
