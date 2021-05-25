5a3664fdc92734467d54fcb84111d80f2d716b58
Brandon King <brandonscottking@gmail.com>
2021-05-24 20:46:09 -0400

    fixed seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -126,8 +126,8 @@ const workoutSeed = [
   },
 ];
 
-Workout.deleteMany({})
-  .then(() => Workout.collection.insertMany(workoutSeed))
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then((data) => {
     console.log(data.result.n + ' records inserted!');
     process.exit(0);
